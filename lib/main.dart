dec61a37365265049ff5718830004515ca9b3437
Lucas de Vargas Souza <lvargas@gvdasa.local>
2019-08-08 13:19:04 -0300

    App confgurada para fazer as requisições async!

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext contex) {
-    return MaterialApp(
+const request = "https://api.hgbrasil.com/finance?format=json&key=<sua-chave-de-api>";
+
+void main() async {
+
+  http.Response response = await http.get(request);
+
+  runApp(
+    MaterialApp(
       title: "Conversor de moedas",
       home: HomePage(),
-    );
-  }
+    ),
+  );
 }
 
 class HomePage extends StatefulWidget {
@@ -20,9 +25,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
-
